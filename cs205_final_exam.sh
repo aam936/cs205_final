14259e01ce38354bac7c7e421347b085bf205ace
aam936 <aam936@nau.edu>
2024-05-07 21:22:51 -0700

    Added screenshot of terminal input/output

--- cs205_final_exam.sh
+++ cs205_final_exam.sh
@@ -10,3 +10,15 @@
 # The spacing and header formatting should match the above formatting description exactly.
 # There should be a comment explaining the purpose of each line in your shell script. 
 # The data file will be passed in to the script as a positional parameter and will not necessarily be called best_pokemon.dat. However, you can assume that any file passed to this script will be formatted exactly the way best_pokemon.dat is formatted.
+
+#!/bin/bash
+filename="$1"
+echo "===== SUMMARY OF DATA FILE ====="
+echo "   File name: $filename"
+total_pokemon=$(awk 'END {print NR}' "$filename")
+echo "   Total Pokemon: $total_pokemon"
+avg_hp=$(awk '{total += $2} END {print total / NR}' "$filename")
+echo "   Avg. HP: $avg_hp"
+avg_attack=$(awk '{total += $3} END {print total / NR}' "$filename")
+echo "   Avg. Attack: $avg_attack"
+echo "===== END SUMMARY ====="
